3adae819dc395f589bf2ea6a128220241a73dd70
vivekds19 <dsvivek092@gmail.com>
2025-10-20 19:28:44 +0530

    count vowels in the string

---
+++ vowels.js
@@ -0,0 +1,10 @@
+const str = "abcdefghijklmnopqrstuvwxyzncjdhoasnbhsbuhwuwhqhbzareirybc";
+
+const vowels = "aeiou";
+let count = 0;
+for (let i = 0; i < str.length; i++) {
+  if (vowels.includes(str[i])) {
+    count++;
+  }
+}
+console.log(count);
