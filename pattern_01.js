ba4a69d1ced34847a94dd410a21dcb0751ed21d9
vivekds19 <dsvivek092@gmail.com>
2025-10-20 19:01:09 +0530

    patterns

---
+++ pattern_01.js
@@ -0,0 +1,4 @@
+const str = "* "
+for (let i = 1; i <= 5; i++) {
+  console.log(str.repeat(i));
+}
---
+++ pattern_2.js
@@ -0,0 +1,4 @@
+const str = "* ";
+for (let i = 5; i >= 1; i--) {
+  console.log(str.repeat(i));
+}
