12e65fe6dadc2f47f5e46ad933ebad3d6c7d9e83
vivekds19 <dsvivek092@gmail.com>
2025-10-21 15:17:29 +0530

    AnonymousFunction

---
+++ ananymous_function.js
@@ -0,0 +1,9 @@
+// anonymousFunction(); 
+// calling the anonymous function before the initialization it gives reference error
+
+let anonymousFunction=Function()
+{
+  console.log("Hello, welcome to the world of JavaScript");
+}
+
+anonymousFunction();
