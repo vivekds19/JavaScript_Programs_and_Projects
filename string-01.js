73d5e6062f4de9fd59bcbd5112dc267f441fbad0
vivekds19 <dsvivek092@gmail.com>
2025-10-21 18:57:01 +0530

    find the character index and unicode value

---
+++ string-01.js
@@ -0,0 +1,9 @@
+const displayMessage = "Hi, welcome to JavaScript programming ";
+
+const char = displayMessage.charAt(5);
+
+console.log(char);
+
+const ascii = displayMessage.charCodeAt(5);
+
+console.log(ascii);
