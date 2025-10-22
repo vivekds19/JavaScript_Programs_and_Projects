ae0da8a22774f3f88811a7d8a52a66f47f7d3e6b
vivekds19 <dsvivek092@gmail.com>
2025-10-22 12:26:35 +0530

    it removes the white spaces in the string

---
+++ trim.js
@@ -0,0 +1,7 @@
+const text = "hi , welcome to javascript                    ";
+
+const textTrimmed = text.trim();
+console.log(textTrimmed);
+console.log(textTrimmed.length);
+console.log(text);
+console.log(text.length);
