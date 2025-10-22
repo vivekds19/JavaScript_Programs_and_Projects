976d8bf85739ee6c0ac1f3d62c3fc3aaa77d7ffb
vivekds19 <dsvivek092@gmail.com>
2025-10-22 13:39:03 +0530

    count  no of words in the string

---
+++ count_words_string.js
@@ -0,0 +1,6 @@
+const words="Hello, world! Welcome to the world of JavaScript programming.";
+const wordsArray=words.split(" ");
+
+const wordCount=wordsArray.length;
+
+console.log("Total number of words:", wordCount);
