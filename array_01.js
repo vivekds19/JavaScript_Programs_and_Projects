669a0d17ed7a4c3f87c826a203fcbfcd811122f0
vivekds19 <dsvivek092@gmail.com>
2025-10-22 20:42:40 +0530

    using let of and let in for loop

--- array_01.js
+++ array_01.js
@@ -1,5 +1,13 @@
 let studentName = ["vivek", "varun", "vishnu", "kriti", "vinay", "dilip"];
-
+/*
 for (let i = 0; i < studentName.length; i++) {
   console.log(studentName[i]);
+}*/
+
+for(let name of studentName){
+    console.log(name);
+}
+
+for(let name in studentName)  {
+  console.log(name);
 }
