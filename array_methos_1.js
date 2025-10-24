317d6c7afd83f6508388eed6a0a4a01a6f9f8f37
vivekds19 <dsvivek092@gmail.com>
2025-10-24 17:48:56 +0530

    splice and slice(string)

--- array_methos_1.js
+++ array_methos_1.js
@@ -3,7 +3,7 @@ let arr = ["html", "css", "react.js", "jquery"];
 
 let updatedCourses = arr.splice(0, 3);
 
-//console.log(updatedCourses)
+console.log(updatedCourses)
 
 // string slice - it extracts a section of a string and returns it as a new string, without modifying the original string.
 
@@ -15,5 +15,4 @@ let capitalized = firstChar + username.slice(1);
 
 console.log(capitalized);
 
-console.log(username);
 
