80b41feeadc155c4706d0de997a83c841a902dbf
vivekds19 <dsvivek092@gmail.com>
2025-10-24 16:54:43 +0530

    push and concat

---
+++ array_methods.js
@@ -0,0 +1,16 @@
+//push- it push the elements to the end of an array
+let arr = [1, 2, 3];
+
+let arr1 = [4, 5, 6];
+
+let arr2 = [7, 8, 9];
+
+arr.push(4);
+
+console.log(arr);
+
+// concat- it combines two or more array and return a new array
+
+let arr3 = arr.concat(arr1, arr2);
+
+console.log(arr3);
