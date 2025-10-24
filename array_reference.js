2b1f256e2e235f907f2f4c851d94d35fe18fcbf2
vivekds19 <dsvivek092@gmail.com>
2025-10-24 21:18:19 +0530

    to avoid array_reference using spread operator

---
+++ array_reference.js
@@ -0,0 +1,26 @@
+let arr1=[1,2,3,4]
+
+let arr2=arr1
+
+console.log("arr1-",arr1);
+console.log("arr2-",arr2);
+
+arr2.push(5);
+
+console.log("updated arr1-",arr1);
+console.log("updated arr2-",arr2);
+
+//spread operator
+
+let arr3=[1,2,3,4];
+
+let arr4=[...arr3];
+
+console.log("arr3-",arr1);
+console.log("arr4-",arr2);
+
+arr4.push(5);
+
+console.log("updated arr3-",arr3);
+console.log("updated arr4-",arr4);
+
