6567286e8229d866819a89ccce72c9ddc6d6a255
vivekds19 <dsvivek092@gmail.com>
2025-10-26 15:17:51 +0530

    using functions to access the  objects

---
+++ objects_1.js
@@ -0,0 +1,11 @@
+const info = {
+  getfunction: function () {
+    console.log("inside the getfunction");
+  },
+  bye() {
+    console.log("inside the bye function");
+  },
+};
+
+info.getfunction();
+info.bye()
