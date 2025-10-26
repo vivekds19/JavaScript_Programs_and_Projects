7bf64914256393884484132140bfd3413e63d758
vivekds19 <dsvivek092@gmail.com>
2025-10-26 15:14:46 +0530

    using objects

---
+++ object.js
@@ -0,0 +1,12 @@
+const info = {
+  name: "vivek",
+  job: "software developer",
+  courses: ["html", "css", "js", "react.js"],
+  "is admin": true,
+};
+
+console.log(info.name);
+
+console.log(info["job"]);
+
+console.log(info["is admin"]);
