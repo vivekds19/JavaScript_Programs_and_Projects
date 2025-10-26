38429439294aae33ece184cafc1280996f374556
vivekds19 <dsvivek092@gmail.com>
2025-10-26 17:27:07 +0530

    shoot-hand property

---
+++ objects_3.js
@@ -0,0 +1,18 @@
+let getobjects = (name, city) => {
+  return {
+    name,
+    city,
+  };
+};
+let student1 = getobjects("Vivek", "Bengaluru");
+
+let student2 = getobjects("arjunreddy", "Hydrabad");
+
+const student = "vik";
+
+const course = "redux";
+
+console.log({ student, course });
+
+console.log(student1);
+console.log(student2);
