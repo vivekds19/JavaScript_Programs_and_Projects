06cd4c4e23f2dc07f43b83afba6f615cf7f230b9
vivekds19 <dsvivek092@gmail.com>
2025-10-27 17:47:04 +0530

    object methods [entries,keys,values]

---
+++ objects_9.js
@@ -0,0 +1,17 @@
+const obj={
+  name:'vivek',
+  age:25,
+  job:'software developer'
+}
+
+let entries=Object.entries(obj)
+
+console.log(entries)
+
+let keys=Object.keys(obj)
+
+console.log(keys)
+
+let values=Object.values(obj)
+
+console.log(values)
