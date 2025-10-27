28d44e82bc8b5a96c56f6ee5ef6c270a9822cddf
vivekds19 <dsvivek092@gmail.com>
2025-10-27 19:27:52 +0530

    Constructor

---
+++ constructor.js
@@ -0,0 +1,13 @@
+function User(name, age) {
+  this.name = name;
+  this.age = age;
+}
+const user1 = new User("vivek", 22);
+const user2 = new User("varun", 23);
+const user3 = new User("vishnu", 24);
+const user4 = new User("vinay", 25);
+
+console.log(user1);
+console.log(user2);
+console.log(user3);
+console.log(user4);
