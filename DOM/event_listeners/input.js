862844edb1f5dafed5c39d1f1928ea041ce0601f
vivekds19 <dsvivek092@gmail.com>
2025-11-03 17:53:10 +0530

    using change, input , focus,keyUp event listeners

---
+++ DOM/event_listeners/input.js
@@ -0,0 +1,10 @@
+let input = document.querySelector(".input");
+
+/*
+input.addEventListener("change", () => {
+  console.log(input.value);
+});
+*/
+input.addEventListener("input", () => {
+  console.log(input.value);
+});
