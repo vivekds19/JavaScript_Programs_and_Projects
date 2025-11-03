cb3a26f8a53f5ce66b8a865c696c4a810fbdb1a8
vivekds19 <dsvivek092@gmail.com>
2025-11-03 13:00:57 +0530

    accesing  and seraching for the Dom elements

---
+++ DOM/index.js
@@ -0,0 +1,13 @@
+let startBtn = document.getElementById("btn-start");
+
+console.log(startBtn);
+
+let stopBtn = document.getElementsByClassName("button stop");
+
+console.log(stopBtn);
+
+let btn = document.querySelectorAll(".button");
+console.log(btn);
+
+let btn_1 = document.querySelector("#btn-stop");
+console.log(btn_1);
