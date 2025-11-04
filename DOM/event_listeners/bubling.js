6a9a5d01f9cc5037376a2d98545ac38eb62b1b07
vivekds19 <dsvivek092@gmail.com>
2025-11-04 16:18:38 +0530

    using event bubbling and event capturing

---
+++ DOM/event_listeners/bubling.js
@@ -0,0 +1,42 @@
+let form = document.querySelector("#form");
+let div = document.querySelector("#div");
+let para = document.querySelector("#p");
+
+// bubbling
+para.addEventListener("click", () => {
+  alert("para tag");
+});
+
+div.addEventListener("click", () => {
+  alert("div tag");
+});
+
+form.addEventListener("click", () => {
+  alert("form tag");
+});
+
+// capturing
+
+para.addEventListener(
+  "click",
+  () => {
+    alert("para tag");
+  },
+  true
+);
+
+div.addEventListener(
+  "click",
+  () => {
+    alert("div tag");
+  },
+  true
+);
+
+form.addEventListener(
+  "click",
+  () => {
+    alert("form tag");
+  },
+  true
+);
