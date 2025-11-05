a35eda4cd4802f2fed700a2060109e1802e61648
vivekds19 <dsvivek092@gmail.com>
2025-11-05 14:23:16 +0530

    whislist app

---
+++ Projects/whislist/index.js
@@ -0,0 +1,8 @@
+let input = document.querySelector(".input");
+
+let addTodoButton = document.querySelector(".button");
+
+addTodoButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("clicked");
+});
