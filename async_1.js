168ce40eaed94c94ea8f15f1433c831a15160783
vivekds19 <dsvivek092@gmail.com>
2025-11-10 17:24:19 +0530

    async function and await  keyword

---
+++ async_1.js
@@ -0,0 +1,7 @@
+async function fetchData() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+  const data = await response.json();
+  console.log(data);
+}
+
+fetchData();
